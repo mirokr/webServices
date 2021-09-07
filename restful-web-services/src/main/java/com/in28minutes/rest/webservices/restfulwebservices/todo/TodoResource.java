22388cbb33c00aef970ca580c0cb0b4d076482f4
Miro <82343950+mirokr@users.noreply.github.com>
2021-09-07 15:44:42 +0100

    Add files via upload

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -3,7 +3,9 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,20 @@ public class TodoResource {
 	private TodoHardcodedService todoService;
 	
 	@GetMapping("/users/{username}/todos")
-	public List<Todo> getAllTodos(@PathVariable String username) throws InterruptedException{
+	public List<Todo> getAllTodos(@PathVariable String username) {
 //		Thread.sleep(3000);
 		return todoService.findAll();
 	}
+	
+	//delete
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable String username, @PathVariable long id){
+		Todo todo = todoService.deleteById(id);
+		if(todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 }
